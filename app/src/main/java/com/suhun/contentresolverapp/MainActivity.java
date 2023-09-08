0b66a23aefc58898daa1cf351268f5e162d59052
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-08 13:28:11 +0800

    1.Add permission in AndroidManifest.xml     <uses-permission android:name="android.permission.READ_CONTACTS"/>     <uses-permission android:name="android.permission.READ_CALL_LOG"/>     <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>     <uses-permission android:name="android.permission.ACCESS_MEDIA_LOCATION"/>
    2.Add userAgreePermission() to check if user agree permission or not.
    (1.)If user already agree then initContentResolver()
    (2.)If user not agree then send requestPermissions() to user

--- app/src/main/java/com/suhun/contentresolverapp/MainActivity.java
+++ app/src/main/java/com/suhun/contentresolverapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.suhun.contentresolverapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
@@ -12,12 +17,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
     private TextView fieldNameResult;
     private ListView fieldContentList;
     private SimpleAdapter simpleAdapter;
     private ArrayList<HashMap<String, String>> data = new ArrayList<>();
     private String[] from = {"content"};
     private int[] to = {R.id.list_content};
+    private ContentResolver contentResolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         initLstView();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED /*grantResults[2] == PackageManager.PERMISSION_GRANTED*/){
+                initContentResolver();
+            }else{
+                finish();
+            }
+        }
     }
 
     private void initView(){
@@ -38,10 +58,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getFieldNameFun(View view){
-
+        if(!userAgreePermission()){
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALL_LOG, Manifest.permission.MANAGE_EXTERNAL_STORAGE}, 123);
+        }else{
+            initContentResolver();
+        }
     }
 
     public void getFieldContentFun(View view){
 
+    }
+
+    private boolean userAgreePermission(){
+        boolean result = false;
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) ==  PackageManager.PERMISSION_GRANTED||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            result = true;
+        }
+
+        return result;
+    }
+
+    private void initContentResolver(){
+        contentResolver = getContentResolver();
     }
 }
