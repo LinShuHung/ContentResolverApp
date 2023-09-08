ef2b7240d43385f61618331e09fc8e4f5bcca596
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-09-08 12:36:32 +0800

    Add layout

--- app/src/main/java/com/suhun/contentresolverapp/MainActivity.java
+++ app/src/main/java/com/suhun/contentresolverapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.suhun.contentresolverapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getFieldNameFun(View view){
+
+    }
+
+    public void getFieldContentFun(View view){
+
     }
 }
